2854d769a5a17c72dc913983ab507faa3e816da6
Luis Hidalgo <hid21002@byui.edu>
2024-01-12 18:25:49 -0500

    up

--- TriangleAdventures/settings.py
+++ TriangleAdventures/settings.py
@@ -84,12 +84,7 @@ WSGI_APPLICATION = 'TriangleAdventures.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
