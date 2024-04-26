89600ce7bf697c1a95b9593a2b80494541c28f80
Luis Hidalgo <hid21002@byui.com>
2024-04-25 21:37:02 -0400

    update

--- static/js/image_script.js
+++ static/js/image_script.js
@@ -5,6 +5,33 @@ const initSlider = () =>{
     const sliderScrollbar = document.querySelector(".images_container .slider-scrollbar")
     const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb")
 
+    scrollbarThumb.addEventListener("mousedown", (e) => {
+        const startX = e.clientX;
+        const thumbPosition = scrollbarThumb.offsetLeft;
+
+        const handleMouseMove = () => {
+            const deltaX = e.clientX - startX;
+            const newThumbPosition = thumbPosition + deltaX;
+
+            const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
+
+            const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+
+            const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft
+
+            scrollbarThumb.style.left = `${boundedPosition}px`;
+            imageList.scrollLeft = scrollPosition;
+        }
+
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp)
+        }
+
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    })
+
 
     slideButtons.forEach(button => {
         button.addEventListener("click", () => {
